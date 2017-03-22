b4f2dd61265df103acac69ee1ef5d4cad3224628
Hoang Vo <hoang.vominh96@gmail.com>
2017-03-22 02:00:46 +0200

    adding pick color, returning the color choosing for the identicon

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,27 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  This module is for 
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
   end
+
+  ## we can do pattern matching inside argument list
+  ## in this case, we take image as our argument, while we are taking image as our argument, we also do some
+  ## pattern matching there
+  def pick_color(%Identicon.Image{hex: [r,g,b | _tail]} = image) do     
+    ## taking all of the image property their, PASS ON to the one after the PIPE, add color to it.
+    %Identicon.Image{image | color: {r,g,b}}
+  end
+
+  def hash_input(input) do 
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list    
+
+    %Identicon.Image{hex: hex}
+  end
+
 end
