69b0be7d7f5e1751f80f12f4f3e965dff64b5cf3
Hoang Vo <hoang.vominh96@gmail.com>
2017-03-23 01:54:15 +0200

    adding pixel map function, it is to build a pixel coordinate for top-left and bottom-right point of each grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -5,8 +5,27 @@ defmodule Identicon do
 
   def main(input) do
     input
-    |> hash_input
-    |> pick_color
+      |> hash_input
+      |> pick_color
+      |> build_grid
+      |> filter_odd_squares
+      |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do 
+    pixel_map = Enum.map grid, fn({_value, index}) -> 
+      horizontal = rem(index, 5)*50
+      vertical = div(index, 5)*50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}      
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do 
     # grid
     #   |> Enum.filter(
@@ -18,6 +37,7 @@ defmodule Identicon do
 
     %Identicon.Image{image | grid: grid}
   end
+
   def build_grid(%Identicon.Image{ hex: hex } = image) do 
     grid = 
       hex
