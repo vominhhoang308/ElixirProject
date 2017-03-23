44a55758dd0d0324a9f9ce422bd7b727f3980d7e
Hoang Vo <hoang.vominh96@gmail.com>
2017-03-23 02:08:42 +0200

    change packagename, erlang edg -> erlang egd, wrong package name

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -24,14 +24,14 @@ defmodule Identicon do
   end
 
   def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
-    image = :edg.create(250, 250)
-    fill = :edg.color(color)
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
 
     Enum.each pixel_map, fn({start , stop}) -> 
-      :edg.filledRectangle(image, start, stop, fill)
+      :egd.filledRectangle(image, start, stop, fill)
     end
 
-    :edg.render(image)
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do 
